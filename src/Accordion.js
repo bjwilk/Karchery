c3b082cbcb27fb5ec21e8d77bd09e7d85219679b
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2023-05-26 15:35:33 -0700

    added FAQ

---
+++ src/Accordion.js
@@ -0,0 +1,126 @@
+import React from "react";
+import "./Accordion.css"; // Import the CSS file
+
+class Accordion extends React.Component {
+  handleAccordionClick(event) {
+    event.target.classList.toggle("active");
+    const panel = event.target.nextElementSibling;
+    panel.classList.toggle("show");
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: Where is the range located?
+        </button>
+        <div className="panel">
+          <p>
+            Lessons are offered at Ohlone Archery: 2301 Verna Court, San
+            Leandro. Unfortunately, East Bay Archery is not equipped to offer
+            off site classes.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: What does the cost cover?
+        </button>
+        <div className="panel">
+          <p>
+            The cost covers everything having to do with archery: bows, arrows,
+            armguards, finger tabs, bales, targets, instruction and lane time.
+            There are no hidden costs.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: Can our group bring food or alcohol?
+        </button>
+        <div className="panel">
+          <p>
+            Both food and alcohol are prohibited at the range. But, San Leandro
+            has some great surrounding food options I am happy to share with
+            you.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: What if our group wants a longer than one hour class or lesson?
+        </button>
+        <div className="panel">
+          <p>
+            Greater than one hour slots can be arranged but must be done ahead
+            of time. Please email me.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: What should we/I bring?
+        </button>
+        <div className="panel">
+          <p>Yourself and maybe a water bottle.</p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: What should participants wear?
+        </button>
+        <div className="panel">
+          <p>
+            Closed toe shoes are preferable. Any pants, shorts or skirts are
+            fine but I advise against very baggy or flowing sleeves. They will
+            get caught in the string. Also, I suggest avoiding long earrings and
+            I caution against trying to shoot with very, very long nails. They
+            may break.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: What if we have to cancel?
+        </button>
+        <div className="panel">
+          <p>
+            Cancellations are free of charge if done with a minimum of 24 hours
+            notice. If less than 24 hours, a cancellation fee of $50 for 3
+            people or less and $70 for 4 people and more will be charged.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: What if we bring more than the number of people we booked for?
+        </button>
+        <div className="panel">
+          <p>
+            We charge per person. For 4 to 10 people, the cost is $45 per
+            person. For 11 and more, the cost is $35 per person. If more than
+            the number agreed upon attend AND shoot, the difference must be
+            paid. If fewer than the agreed upon number attend, we can issue a
+            refund for the missing participants.
+          </p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: How many people can participate in a group class?
+        </button>
+        <div className="panel">
+          <p>We can accommodate up to 22 participants.</p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: Is the range ADA accessible?
+        </button>
+        <div className="panel">
+          <p>Yes, it is.</p>
+        </div>
+
+        <button className="accordion" onClick={this.handleAccordionClick}>
+          Q: Is there an age requirement?
+        </button>
+        <div className="panel">
+          <p>Yes, 14 years of age and up. No one is too old!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Accordion;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import banner from './img/banner.jpeg'
-import styled from 'styled-components';
+import React from "react";
+import banner from "./img/banner.jpeg";
+import styled from "styled-components";
+import Accordion from "./Accordion";
 
 const Navbar = styled.nav`
   background-color: black;
@@ -9,7 +10,6 @@ const Navbar = styled.nav`
   left: 0;
   width: 100%;
   height: 50px;
- 
 `;
 
 const Banner = styled.img`
@@ -17,8 +17,7 @@ const Banner = styled.img`
   height: auto;
   margin-top: 20px;
   padding-top: 50px;
-  `;
-
+`;
 
 const Container = styled.div`
   display: flex;
@@ -27,16 +26,15 @@ const Container = styled.div`
 `;
 
 const Footer = styled.footer`
-background-color: #222227;
+  background-color: #222227;
   padding: 20px;
   marging: 20px;
   bottom: 0;
 `;
 
-
 const Button = styled.button`
-  background-color: #FF2400;
-  color: #53F4E6;
+  background-color: #ff2400;
+  color: #53f4e6;
   font-size: 16px;
   border: 1px solid gold;
   padding: 8px 16px;
@@ -55,60 +53,101 @@ const Button = styled.button`
   }
 `;
 
+const Lower = styled.div`
+  background-color: #a5d0cc;
+  padding: 20px;
+  margin: 20px;
+`;
+
 const Middle = styled.div`
-background-color: #A5D0CC;
-padding: 20px;
-margin: 20px
-`
+  background-color: #a5d0cc;
+  padding: 20px;
+  margin: 20px;
+`;
 
 const Upper = styled.div`
-background-color: #222227;
-padding: 10px;
-margin: 5px;
-`
+  background-color: #222227;
+  padding: 10px;
+  margin: 5px;
+`;
 
 const P = styled.p`
-color: white;
-font-size: 20px
-`
+  color: white;
+  font-size: 20px;
+`;
 
 function App() {
   return (
     <>
       <Navbar>
-            <Container>
-              <a href="https://eastbayarchery.simplybook.me">
-                <Button>Book Event</Button>
-              </a>
-            </Container>
+        <Container>
+          <a href="https://eastbayarchery.simplybook.me">
+            <Button>Book Event</Button>
+          </a>
+        </Container>
       </Navbar>
 
-          <Upper>
-            <Banner src={banner} alt="Kari"/>
-           <h1 style={{color: '#A5D0CC', fontFamily: 'Arial, sans-serif'}}>Bio</h1>
-            <P>I began my archery practice in 2012 in Los Angeles, CA. A wonderful group of passionate archers ran a free program to introduce archery to the community. It did not take long before I became a regular. When I moved to the Bay Area, I knew I had to take archery with me.</P>
-            <P>Archery is less a sport to me and more a practice. I use archery to clear and steady my mind, to check in with my posture and my focus. It can be an excellent remedy to a bad day or a perfect morning start.</P>
-            <P>The Bay Area has a fantastic archery community with many opportunities to learn and practice as well as compete for those who are interested.</P>
-          </Upper>
+      <Upper>
+        <Banner src={banner} alt="Kari" />
+        <h1 style={{ color: "#A5D0CC", fontFamily: "Arial, sans-serif" }}>
+          Bio
+        </h1>
+        <P>
+          I began my archery practice in 2012 in Los Angeles, CA. A wonderful
+          group of passionate archers ran a free program to introduce archery to
+          the community. It did not take long before I became a regular. When I
+          moved to the Bay Area, I knew I had to take archery with me.
+        </P>
+        <P>
+          Archery is less a sport to me and more a practice. I use archery to
+          clear and steady my mind, to check in with my posture and my focus. It
+          can be an excellent remedy to a bad day or a perfect morning start.
+        </P>
+        <P>
+          The Bay Area has a fantastic archery community with many opportunities
+          to learn and practice as well as compete for those who are interested.
+        </P>
+      </Upper>
 
-       <Middle>
-            <h1>Individual Lessons and Corporate Events</h1>
-            <p style={{fontSize: '20px'}}>Whether you have always wished to learn archery, think it might be a fun experience or want to have a team building event, I offer Private Lessons and Group Events at <u>Ohlone Archery, San Leandro, CA</u>.</p>
-            <ul>
-              <li><span><strong>Events can accommodate up to 24 participants</strong>.</span></li>
-              <li><span><strong>All participants must be 14 years of age or older</strong>.</span></li>
-            </ul>
+      <Middle>
+        <h1>Individual Lessons and Corporate Events</h1>
+        <p style={{ fontSize: "20px" }}>
+          Whether you have always wished to learn archery, think it might be a
+          fun experience or want to have a team building event, I offer Private
+          Lessons and Group Events at <u>Ohlone Archery, San Leandro, CA</u>.
+        </p>
+        <ul>
+          <li>
+            <span>
+              <strong>Events can accommodate up to 24 participants</strong>.
+            </span>
+          </li>
+          <li>
+            <span>
+              <strong>All participants must be 14 years of age or older</strong>
+              .
+            </span>
+          </li>
+        </ul>
+      </Middle>
 
-        </Middle>
+      <Lower>
+        <h1>FAQ</h1>
+        <Accordion />
+      </Lower>
 
       <Footer>
         <div>
-          <a href="mailto:napoli.kari@gmail.com" style={{color: 'gold'}}>email: napoli.kari@gmail.com</a>
-            <a href="tel:+17075831707"><p style={{color: 'gold'}}>Phone: 707-583-1707</p></a>
+          <a href="mailto:napoli.kari@gmail.com" style={{ color: "gold" }}>
+            email: napoli.kari@gmail.com
+          </a>
+          <a href="tel:+17075831707">
+            <p style={{ color: "gold" }}>Phone: 707-583-1707</p>
+          </a>
         </div>
         <a href="https://eastbayarchery.simplybook.me">
-                <Button>Book Event</Button>
-              </a>
+          <Button>Book Event</Button>
+        </a>
       </Footer>
     </>
   );
